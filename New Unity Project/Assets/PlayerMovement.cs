3ecb610adea44cf37cb2f6c4bf7c7a939346758a
Damian Tomlinson <dam982010@yahoo.com>
2021-02-04 16:13:43 -0500

    I modified the PlayerMovement.cs script and I created a new script called CameraFollow.cs
    Inside the PlayerMovement script I added the ablility to crouch to the Player. Also in the new CameraFollow script hence the name I enabled the camera to follow the Players movements so if the player goes right the camera follows to the right.

--- New Unity Project/Assets/PlayerMovement.cs
+++ New Unity Project/Assets/PlayerMovement.cs
@@ -3,6 +3,9 @@
     # 
     # 2: Title: "HOLD JUMP KEY TO JUMP HIGHER - 2D PLATFORMER CONTROLLER - UNITY TUTORIAL" | Author: Blackthornprod | Source: https://www.youtube.com/watch?v=j111eKN8sJw&list=WL&index=28 | Date retrieved: 2/3/2021 @ 2:05pm
     #
+    #
+    # 3: Title: "2D Movement in Unity (Tutorial)" | Author: Brackeys | Source: https://www.youtube.com/watch?v=dwcT-Dch0bA&t=1036s, https://github.com/Brackeys/2D-Movement/blob/master/2D%20Movement/Assets/CharacterController2D.cs | Date retrieved: Date retrieved: 1/27/2021 @ 8:30pm
+    #
 */
 
 // #2: ####################################################################################################################################################
@@ -11,6 +14,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour{
+
+// #3: ####################################################################################################################################################
+	[Range(0, 1)] [SerializeField] private float m_CrouchSpeed = .36f;			// Amount of maxSpeed applied to crouching movement. 1 = 100%
+	[SerializeField] private Transform m_GroundCheck;							// A position marking where to check if the player is grounded.
+	[SerializeField] private Transform m_CeilingCheck;							// A position marking where to check for ceilings
+	[SerializeField] private Collider2D m_CrouchDisableCollider;				// A collider that will be disabled when crouching
+
+	const float k_CeilingRadius = .2f; // Radius of the overlap circle to determine if the player can stand up
+// ####################################################################################################################################################
 
     private Rigidbody2D rb;
     public float speed; // Initializing how fast the player moves.
@@ -23,7 +35,8 @@ public class PlayerMovement : MonoBehaviour{
     private float jumpTimeCounter; // This code is used as a Increment of how many times the user jumps.
     public float jumpTime; //  Initializing the amount of pressure holding down the spacebar to make player jump higher.
     private bool isJumping; // Checks if player is jumping.
-
+    public bool crouch; 
+   
     void Start(){
         rb = GetComponent<Rigidbody2D>(); //This line of code is just calling the Rigidbody2D Component which is attached to my player.
     }
@@ -34,6 +47,34 @@ public class PlayerMovement : MonoBehaviour{
         rb.velocity = new Vector2(moveInput * speed, rb.velocity.y); // This code implements speed to the player.
     }
 
+   // #3: ####################################################################################################################################################
+        public void Move(float move, bool crouch, bool jump){
+		// If crouching, check to see if the character can stand up
+		if (!crouch)
+		{
+			// If the character has a ceiling preventing them from standing up, keep them crouching
+			if (Physics2D.OverlapCircle(m_CeilingCheck.position, k_CeilingRadius, whatIsGround))
+			{
+				crouch = true;
+			}
+		}
+        // If crouching
+		if (crouch){
+			// Reduce the speed by the crouchSpeed multiplier
+			   move *= m_CrouchSpeed;
+
+			    // Disable one of the colliders when crouching
+				if (m_CrouchDisableCollider != null)
+					m_CrouchDisableCollider.enabled = false;
+			} 
+            else{
+				// Enable the collider when not crouching
+				if (m_CrouchDisableCollider != null)
+					m_CrouchDisableCollider.enabled = true;
+			}
+    }
+    // ####################################################################################################################################################
+    
     void Update(){
         isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
 
@@ -66,7 +107,14 @@ public class PlayerMovement : MonoBehaviour{
         if(Input.GetKeyUp(KeyCode.Space)){ // This if block just detects if the player is holding down on spacebar if not then player will not jump.
             isJumping = false;
         }
-
+        // #3: ####################################################################################################################################################
+        if (Input.GetButtonDown("Crouch")){
+			crouch = true;
+		} 
+        else if (Input.GetButtonUp("Crouch")){
+			crouch = false;
+		}
+        // ####################################################################################################################################################
     }
 }
 // ####################################################################################################################################################
