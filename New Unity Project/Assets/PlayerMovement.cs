a3b534fee9d0ff12c5183ea57672cd70459ca068
Damian Tomlinson <dam982010@yahoo.com>
2021-02-03 20:08:40 -0500

    I found a better solution for the movement of the charater
    I also modified the players jump ability by whenever the user holds down the spacebar it enables the player to jump higher.

--- New Unity Project/Assets/PlayerMovement.cs
+++ New Unity Project/Assets/PlayerMovement.cs
@@ -1,72 +1,72 @@
-﻿/*  #
-    # 1: Title: "2D Movement in Unity (Tutorial)" | Author: Brackeys | Source: 	https://www.youtube.com/watch?v=dwcT-Dch0bA&list=WL&index=16&t=826s | Date retrieved: 1/27/2021 @ 9:15pm
+﻿/*  # 1: Title: "2D Character Movement in Unity / 2021" | Author: Distorted Pixel Studios | Source: https://www.youtube.com/watch?v=n4N9VEA2GFo&list=WL&index=26&t=2s | Date retrieved: 1/29/2021 @ 7:30pm
     #
     # 
-    # 2: Title: "2D Character Movement in Unity / 2021" | Author:
-Distorted Pixel Studios | Source: https://www.youtube.com/watch?v=n4N9VEA2GFo&list=WL&index=26&t=2s | Date retrieved: 1/29/2021 @ 7:30pm
+    # 2: Title: "HOLD JUMP KEY TO JUMP HIGHER - 2D PLATFORMER CONTROLLER - UNITY TUTORIAL" | Author: Blackthornprod | Source: https://www.youtube.com/watch?v=j111eKN8sJw&list=WL&index=28 | Date retrieved: 2/3/2021 @ 2:05pm
     #
 */
 
-
-  // #1: ####################################################################################################################################################
+// #2: ####################################################################################################################################################
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
-{ 
-    public CharacterController2D controller;// This line of code is getting Reference from the PlayerMovement script to the CharacterController2D script to move the player.
-    public float runSpeed = 40f; //  Initializing the amount of speed added when the player moves.
-    public float fowardMove = 0f;//  Initializing how fast the character moves.
-    public bool crouch = false; // Whether or not the player is crouched.
-    // ####################################################################################################################################################
+public class PlayerMovement : MonoBehaviour{
 
-    // #2: ####################################################################################################################################################
-    public Rigidbody2D rb;
-    public float jumpForce = 20f; //  Initializing the amount of force added when the player jumps.
-   
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>(); //This line of code is just calling the Rigidbody2D Component thats native to unity.
+    private Rigidbody2D rb;
+    public float speed; // Initializing how fast the player moves.
+    public float jumpForce; //  Initializing the amount of force added when the player jumps.
+    private float moveInput; // This code detects Weather the user is holding down the left or right arrow keys.
+    private bool isGrounded; // Checks if player is on the ground.
+    public Transform feetPos; // This code will detect if the players feet is on the ground so it can jump.
+    public float checkRadius; // This code just enables the  user to change how large they want the radius around the players feet to be.
+    public LayerMask whatIsGround; //  Initializing what will be consider solid ground for the player to move on.
+    private float jumpTimeCounter; // This code is used as a Increment of how many times the user jumps.
+    public float jumpTime; //  Initializing the amount of pressure holding down the spacebar to make player jump higher.
+    private bool isJumping; // Checks if player is jumping.
+
+    void Start(){
+        rb = GetComponent<Rigidbody2D>(); //This line of code is just calling the Rigidbody2D Component which is attached to my player.
     }
-    // ####################################################################################################################################################
-    
-     void Update()
-    {  // #1: ####################################################################################################################################################
-        fowardMove = Input.GetAxisRaw("Horizontal") * runSpeed; // This line of code determines how the charater moves on the horizontal axis using the left & right arrow keys.
 
-         if(Input.GetButtonDown("Crouch")) // This code enables the down arrow key to let the charater crouch.
-        {
-            crouch = true;
-        }
-        else if (Input.GetButtonUp("Crouch")) // This else if block also means if down arrow key is not pressed down to not crouch chracter.
-        {
-            crouch = false;
-        }
-        // ####################################################################################################################################################
+    void FixedUpdate(){
+        moveInput = Input.GetAxisRaw("Horizontal"); // This line of code determines how the charater moves on the horizontal axis using the left & right arrow keys.
 
-    // #2: ####################################################################################################################################################
-        if(Input.GetButtonDown("Jump") && Mathf.Abs(rb.velocity.y) < 0.001f){ // This code enables the spacebar to activate chracter jump when pressed down.
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y); // This code implements speed to the player.
+    }
+
+    void Update(){
+        isGrounded = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
+
+        if(moveInput > 0){ 
+            transform.eulerAngles = new Vector3(0, 0, 0); // This code enables the player to face right when user is moving right.
+        }
+        else if(moveInput < 0){
+            transform.eulerAngles = new Vector3(0, 180, 0); // This code enables the player to face left when user is moving left.
+        }
+        // #1: ####################################################################################################################################################
+         if(Input.GetButtonDown("Jump") && Mathf.Abs(rb.velocity.y) < 0.001f){ // This code enables the spacebar to activate chracter jump when pressed down.
             rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);// The code also stops charater from jumping Continuously in the air.
+        // ####################################################################################################################################################
+            isJumping = true;
+            jumpTimeCounter = jumpTime;
+            rb.velocity = Vector2.up * jumpForce;
+         }
+
+        if(Input.GetKey(KeyCode.Space) && isJumping == true){ // This code checks how long the user is holding down the spacebar.
+
+            if(jumpTimeCounter > 0){ // This if block is saying if jumpTimeCounter is greater than 0 then the player can keep jumping higher.
+            rb.velocity = Vector2.up * jumpForce;
+            jumpTimeCounter -= Time.deltaTime;// Also this piece of code will The deincrement the jumpTimeCounter so that the player falls to the ground.
+            }
+            else{
+                isJumping = false;
+            }
         }
-    }
-    // ####################################################################################################################################################
 
-     void FixedUpdate()
-    {
-      // #1: ####################################################################################################################################################
-        // Make the character move
-        controller.Move(fowardMove * Time.fixedDeltaTime, crouch, false);
-       
-    }
-    // ####################################################################################################################################################
+        if(Input.GetKeyUp(KeyCode.Space)){ // This if block just detects if the player is holding down on spacebar if not then player will not jump.
+            isJumping = false;
+        }
 
-    // #2: ####################################################################################################################################################
-        // Makes character Jump 
-    void Jump(){
-        Vector2 movement = new Vector2(rb.velocity.x, jumpForce);
-        
-        rb.velocity = movement;
     }
 }
-    // ####################################################################################################################################################
+// ####################################################################################################################################################
