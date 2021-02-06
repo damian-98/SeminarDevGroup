c36fb9f5d009beb7bd031cba462def84e796c0ff
RyzzleMeThis <77414356+RyzzleMeThis@users.noreply.github.com>
2021-02-06 15:11:54 -0600

    Coin Implementation
    I did coin implementation which allows character to collect coins

---
+++ New Unity Project/Assets/coinScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coinScript : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        ScoreTextScript.coinAmount += 1;
+        Destroy(gameObject);
+    }
+}
