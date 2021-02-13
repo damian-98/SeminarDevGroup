706861cd2b754ffa67dd71ec473c13f4730e5455
Damian Tomlinson <dam982010@yahoo.com>
2021-02-12 20:15:36 -0500

    I added a health bar to the enemy also added enemy damage towards the enemy.

---
+++ New Unity Project/Assets/EnemyAction.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAction : MonoBehaviour
+{
+   public float Hitpoints;
+   public float MaxHitpoints = 5;
+   public HealthBar Healthbar;
+
+    void Start()
+    {
+        Hitpoints = MaxHitpoints;
+        Healthbar.SetHealth(Hitpoints, MaxHitpoints);
+    }
+
+   public void TakeHit(float damage)
+   {
+       Hitpoints -= damage;
+       Healthbar.SetHealth(Hitpoints, MaxHitpoints);
+
+       if (Hitpoints <= 0)
+       {
+        Destroy(gameObject);   
+       }
+   }
+}
